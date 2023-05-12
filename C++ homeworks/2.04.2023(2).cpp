c55c27a59cee8bc7c13c64b1c20dc8b148336a5f
Ziya Hacili <mziyahajl@mail.ru>
2023-05-12 14:42:56 +0400

    new homework

---
+++ C++ homeworks/2.04.2023(2).cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+#pragma region Part1
+//    const int size = 100;
+//    char arr[size]{};
+//    cin.getline(arr, size);
+//
+//
+//    for (int i = 0; i < size; ++i) {
+//        if (arr[i] == ' ')
+//            arr[i] = '\t';
+//    }
+//
+//    for (int i = 0; i < size; ++i) {
+//        cout << arr[i];
+//    }
+#pragma endregion Part1
+#pragma region Part2
+//    int countAlphabet{};
+//    int countNumbers{};
+//    int countOther{};
+//    const int size = 100;
+//    char arr[size]{};
+//    cin.getline(arr,size);
+//
+//    for (int i = 0; i < size; ++i) {
+//        cout << arr[i];
+//        if (arr[i] >= 65 && arr[i] <=90 || arr[i] >=97 && arr[i] <= 122)
+//            countAlphabet++;
+//        if (arr[i] >= 48 && arr[i] <=57)
+//            countNumbers++;
+//        if (arr[i] >= 32 && arr[i] <= 47 || arr[i] >=58 && arr[i] <= 64 || arr[i] >= 91 && arr[i] <= 96 || arr[i] >=123 && arr[i] <= 126)
+//            countOther++;
+//    }
+//    cout <<endl<< "Count alphabet: " << countAlphabet;
+//    cout << endl << "Count number: " << countNumbers;
+//    cout << endl << "Count other elements: " << countOther;
+#pragma endregion Part2
+#pragma region Part3
+//    const int size = 100;
+//    int countWords = 0;
+//    char arr[size]{};
+//    cin.getline(arr,size);
+//    for (int i = 0; i < size; ++i) {
+//        cout << arr[i];
+//        if (arr[i] == ' ')
+//            countWords++;
+//    }
+//    cout << endl << "Words count: " << countWords+1;
+#pragma endregion Part3
+#pragma region Part4
+//    int size = 0;
+//    cout << "Enter size: "; cin >> size;
+//    char *arr = new char[size]{};
+//    for (int i = 0; i < size; ++i) {
+//        cin >> arr[i];
+//    }
+//
+//    for (int i = 0; i < size; ++i) {
+//        if (arr[i] == arr[size-i-1])
+//            cout << "Palindrome";
+//        else
+//            cout << "Not palindrome";
+//            break;
+//    }
+#pragma endregion Part4
+
+    return 0;
+}
