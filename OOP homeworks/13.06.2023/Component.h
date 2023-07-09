ee65e7d835f9dcb4bc0e2ae4ea0fdfb7f5a7c868
Ziya Hacili <mziyahajl@mail.ru>
2023-07-09 14:34:14 +0400

    13.06.2023

---
+++ OOP homeworks/13.06.2023/Component.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+
+class Component {
+public:
+    string make;
+    string model;
+
+    Component() = default;
+    Component(string &make, string &model);
+
+    void info() const;
+
+
+};
+
+
