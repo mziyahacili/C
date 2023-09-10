3c6a5f81e0a9b9cd5c45ee1cc2332079a94b5352
Ziya Hacili <mziyahajl@mail.ru>
2023-09-10 22:10:32 +0400

    3.09.2023

---
+++ C# homeworks/3.09.2023(2)/3.09.2023(2)/Program.cs
@@ -0,0 +1,511 @@
+﻿#region Part1
+
+/*
+using System.ComponentModel.Design;
+
+try
+{
+    int dollar = int.Parse(Console.ReadLine());
+    int cents = int.Parse(Console.ReadLine());
+
+    Product tv = new Product(dollar,cents);
+    tv.Info();
+    tv.Less(50,30);
+    
+    tv.Info();
+    
+}
+
+catch (MoneyException ex)
+{
+    Console.WriteLine(ex);
+}
+
+
+abstract class Money
+{
+    private int _dollars;
+    private int _cent;
+    
+    public int Dollars
+    {
+        get => _dollars;
+        set
+        {
+            if (value < 0)
+                throw new MoneyException("Ошибка. Деньги не могут быть отрицательными");
+            _dollars = value;  
+        }
+    }
+    public int Cent
+    {
+        get => _cent;
+        set
+        {
+            if (value < 0)
+                throw new MoneyException("Ошибка. Деньги не могут быть отрицательными");
+            if (value >= 100)
+            {
+                Dollars += value / 100;
+                _cent = value % 100;
+            }
+            else
+                _cent = value;
+
+        }
+    }
+
+}
+
+class Product : Money
+{
+    public Money Money { get; set; }
+    
+    
+    public Product(int dollars, int cent)
+    {
+        Dollars = dollars;
+        Cent = cent;
+    }
+
+    public void Less(int dollar, int cent)
+    {
+        Dollars -= dollar;
+        Cent -= cent;
+    }
+    
+    
+    public void Info()
+    {
+        Console.WriteLine($"{Dollars} Dollar {Cent} cent");
+    }
+
+   
+}
+
+class MoneyException : Exception
+{
+    public string Message { get; private set; }
+
+    public MoneyException(string message) : base(message)
+    {
+        Message = message;
+    }
+
+    public override string ToString()
+    {
+        return $"Error message: {Message}";
+    }
+}
+*/
+
+#endregion
+
+#region Part2
+
+/*
+using System.Diagnostics.Tracing;
+
+
+
+Automobile automobile = new Automobile("Car", "Color");
+Microwave microwave = new Microwave("Microwave", "White");
+Kettle kettle = new Kettle("Kettle", "Black");
+Steamship steamship = new Steamship("Steamship", "Pink");
+
+automobile.Show();
+automobile.Desc();
+
+abstract class Device
+{
+    public abstract string Name { get; set; }
+    public abstract string Color { get; set; }
+
+
+
+
+    public virtual void Sound(){}
+    public virtual void Show(){}
+    public virtual void Desc(){}
+}
+
+
+class Kettle : Device
+{
+    private string _name;
+    private string _color;
+
+    
+    public override string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            else
+                _name = value;
+        }
+
+    }
+
+    public override string Color 
+    {   get=>_color;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            else
+                _color = value;
+        }
+    }
+
+    public Kettle(string name, string color)
+    {
+        Name = name;
+        Color = color;
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine("Makes a whistle sound");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("A teapot is a source of comfort and aromatic tea.");
+    }
+}
+
+class Microwave : Device
+{
+    private string _name;
+    private string _color;
+    
+    public override string Name { 
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            _name = value;
+        }
+    }
+
+    public override string Color
+    {
+        get=>_color;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            _color = value;
+        }
+        
+    }
+    
+    public Microwave(string name, string color)
+    {
+        Name = name;
+        Color = color;
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine("Sound microwawe");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("The microwave is a source of quick and tasty cooking.");
+    }
+}
+
+class Automobile : Device
+{
+    private string _name;
+    private string _color;
+    
+    public override string Name { 
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            _name = value;
+        }
+    }
+
+    public override string Color
+    {
+        get=>_color;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            _color = value;
+        }
+        
+    }
+    
+    public Automobile(string name, string color)
+    {
+        Name = name;
+        Color = color;
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine("Car sound");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("A car means freedom of movement and adventure.");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+}
+
+
+class Steamship : Device
+{
+    private string _name;
+    private string _color;
+    
+    public override string Name { 
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            _name = value;
+        }
+    }
+
+    public override string Color
+    {
+        get=>_color;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                throw new Exception("String is empty!");
+            _color = value;
+        }
+        
+    }
+    
+    public Steamship(string name, string color)
+    {
+        Name = name;
+        Color = color;
+    }
+    
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+
+    public override void Sound()
+    {
+        Console.WriteLine("Sound steamship");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("Steamboat - a symbol of sailing and adventure on open waters");
+    }
+}
+*/
+
+
+
+#endregion
+
+#region Part3
+
+/*using System.Threading.Channels;*/
+
+/*
+abstract class MusicalInstrument
+{
+    
+    public abstract void Sound();
+    public abstract void Show();
+    public abstract void Desc();
+    public abstract void History();
+    
+    
+}
+
+class Violin : MusicalInstrument
+{
+    public string Name { get; set; } = "Violin";
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} sound");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("Elegant and melodic, the violin is a stringed musical instrument with a long neck and bow.");
+    }
+
+    public override void History()
+    {
+        Console.WriteLine("The violin was developed in the 16th century in Italy and has since become one of the most popular instruments in the world.");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+}
+
+
+
+class Trombone : MusicalInstrument
+{
+    public string Name { get; set; } = "Trombone";
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} sound");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("Description: The trombone is a brass instrument with a long pipe and a retractable sectional comb that changes the pitch of the sound.");
+    }
+
+    public override void History()
+    {
+        Console.WriteLine("The trombone has been known since the Renaissance and remains an important part of orchestral music.");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+}
+
+
+
+class Ukulele : MusicalInstrument
+{
+    public string Name { get; set; } = "Ukulele";
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} sound");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("The ukulele is a small stringed instrument with four strings that creates bright and cheerful sounds.");
+    }
+
+    public override void History()
+    {
+        Console.WriteLine("The ukulele was developed in Hawaii in the 19th century and quickly gained popularity due to its unique sonority.");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+}
+
+
+class Cello : MusicalInstrument
+{
+    public string Name { get; set; } = "Cello";
+
+    public override void Sound()
+    {
+        Console.WriteLine($"{Name} sound");
+    }
+
+    public override void Desc()
+    {
+        Console.WriteLine("The cello is a large stringed instrument that is played with a bow and produces a rich, deep sound.");
+    }
+
+    public override void History()
+    {
+        Console.WriteLine("The cello was developed in the 16th century and became an integral part of symphony orchestras and chamber music.");
+    }
+
+    public override void Show()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+}
+*/
+
+
+
+#endregion
+
+
+#region Part4
+
+/*
+using System.Runtime.CompilerServices;
+
+abstract class Worker
+{
+    public abstract void Print();
+}
+
+class Security : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Security");
+    }
+}
+
+class Manager : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Manager");
+    }
+}
+
+class Engineer : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("Engineer");
+    }
+}
+
+class President : Worker
+{
+    public override void Print()
+    {
+        Console.WriteLine("President");
+    }
+}
+*/
+
+
+
+#endregion
+
---
+++ C# homeworks/3.09.2023/3.08.2023/Program.cs
@@ -0,0 +1,353 @@
+﻿
+
+
+//3.09.2023
+
+#region Part3
+/*
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+
+try
+{
+
+    IEnumerable<string> book = new[] { "Books", "Robert", "Maria"};
+    BookList books = new BookList(book);
+    
+    IEnumerable<string> book2 = new[] { "Books", "Robert", "Marcus"};
+    BookList books2 = new BookList(book2);
+
+    Console.WriteLine(books == books2);
+    Console.WriteLine(books != books2);
+
+    
+    
+    
+    books.Info();
+    Console.WriteLine();
+    books2.Info();
+
+    books = books - " ";
+    books.Info();
+
+
+}
+catch(CheckNameException ex)
+{
+    Console.WriteLine(ex);
+}
+
+
+class BookList
+{
+    public List<string> Books { get;  set; }
+
+    private int _count;
+    public BookList(IEnumerable<string> books)
+    {
+        Books = new List<string>(books);
+    }
+    
+    public int Count
+    {
+        get => Books.Count();   
+        set => _count = Books.Count; 
+    }
+    
+
+
+    public int Size()
+    {
+        return Books.Count();
+    }
+
+
+
+
+    
+    public string this[int index]
+    {
+        get
+        {
+            return Books[index];
+        }
+        set
+        {
+            Books[index] = value;
+        }
+    }
+
+    public void Search(string nameSearch)
+    {
+        for (int i = 0; i < Books.Count; i++)
+        {
+            if (nameSearch == Books[i] || nameSearch == Books[i])
+            {
+                Console.WriteLine(Books[i]);
+            }
+        }
+    }
+
+    public void Info()
+    {
+        for (int i = 0; i < Books.Count; i++)
+        {
+            Console.WriteLine($"{i+1}.{Books[i]}");
+        }
+    }
+
+   
+    
+    public static BookList operator + (BookList b, string book)
+    {
+        if (string.IsNullOrEmpty(book) || string.IsNullOrWhiteSpace(book))
+            throw new CheckNameException("Error. Write book name!");
+        b.Books.Add(book);
+        return b;
+    }
+
+    public static BookList operator -(BookList b, string book)
+    {
+        if (string.IsNullOrEmpty(book) || string.IsNullOrWhiteSpace(book))
+            throw new CheckNameException("Error. Write book name!");
+        for (int i = 0; i < b.Count; i++)
+        {
+            if (b[i] == book)
+                b.Books.Remove(book);
+        }
+           
+        return b;
+
+    }
+    
+    public static int operator == (BookList b1, BookList b2)
+    {
+        int count = 0;
+        int j = 0;
+
+        for (int i = 0; i < b1.Count; i++)
+        {
+            if (b1.Books[i] == b2.Books[j])
+                count++;
+            j++;
+        }
+
+        return count;
+    }
+
+    public static int operator != (BookList b1, BookList b2)
+    {
+        int count = 0;
+        int j = 0;
+
+        for (int i = 0; i < b1.Count; i++)
+        {
+            if (b1.Books[i] != b2.Books[j])
+                count++;
+            j++;
+        }
+
+        return count;
+    }
+}
+
+class CheckNameException : Exception
+{
+
+    public string Message { get; private set; }
+
+    public CheckNameException(string message) : base(message)
+    {
+        Message = message;
+    }
+
+    public override string ToString()
+    {
+        return $"Error message: {Message}";
+    }
+}
+*/
+
+#endregion
+
+#region Part1
+
+/*
+
+string name = Console.ReadLine();
+int year = int.Parse(Console.ReadLine());
+string number = Console.ReadLine();
+string email = Console.ReadLine();
+int staffCount = int.Parse(Console.ReadLine());
+
+Journal journal = new Journal(name,year,number,email,staffCount);
+
+Console.WriteLine(journal);
+
+journal.StaffCount += 10;
+
+Console.WriteLine(journal);
+
+
+
+class Journal
+{
+    public string Name { get; set; }
+    public int CreateYear { get; set; }
+    public string Number { get; set; }
+    public string Email { get; set; }
+    public int StaffCount { get; set; }
+
+
+    public static int operator +(Journal journal, int value)
+    {
+        return journal.StaffCount += value;
+    }
+
+    public static int operator -(Journal journal, int value)
+    {
+        return journal.StaffCount -= value;
+    }
+
+    public static bool operator ==(Journal j1, Journal j2)
+    {
+        return j1.StaffCount == j2.StaffCount;
+    }
+    
+    public static bool operator !=(Journal j1, Journal j2)
+    {
+        return j1.StaffCount != j2.StaffCount;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Journal)
+        {
+            Journal j = obj as Journal;
+            return j.StaffCount == StaffCount;
+        }
+
+        return false;
+    }
+    public Journal(string name, int createYear, string number, string email, int staffCount)
+    {
+        Name = name;
+        CreateYear = createYear;
+        Number = number;
+        Email = email;
+        StaffCount = staffCount;
+    }
+    
+
+    public override string ToString()
+    {
+        return $"Journal name: {Name}\n" +
+               $"Journal year: {CreateYear}\n" +
+               $"Personal number: {Number}\n" +
+               $"Email: {Email}\n" +
+               $"Staff count: {StaffCount}\n";
+    }
+}
+
+
+
+ */
+
+#endregion
+
+#region Part2
+
+/*
+using System.Threading.Channels;
+
+string name = Console.ReadLine();
+string address = Console.ReadLine();
+string description =Console.ReadLine();
+string number = Console.ReadLine();
+string email = Console.ReadLine();
+double area = double.Parse(Console.ReadLine());
+
+Magazine magazine = new Magazine(name, address, description, number, email,area);
+
+
+class Magazine
+{
+    public string Name { get; set; }
+    public string Address{  get; set; }
+    public string Description { get; set; }
+    public string Number { get; set; }
+    public string Email { get; set; }
+    
+    public double Area { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Magazine)
+        {
+            Magazine m = obj as Magazine;
+            return m.Area == Area;
+        }
+
+        return false;
+    }
+
+    public static double operator +(Magazine m, double value)
+    {
+        return m.Area += value;
+    }
+
+    public static double operator -(Magazine m, double value)
+    {
+        return m.Area -= value;
+    }
+
+    public static bool operator ==(Magazine m1, Magazine m2)
+    {
+        return m1.Area == m2.Area;
+    }
+    
+    public static bool operator !=(Magazine m1, Magazine m2)
+    {
+        return m1.Area != m2.Area;
+    }
+    
+    public Magazine(string name, string address, string description, string number, string email, double area)
+    {
+        Name = name;
+        Address = address;
+        Description = description;
+        Number = number;
+        Email = email;
+        Area = area;
+    }
+    
+
+    public override string ToString()
+    {
+        return $"Magazine name: {Name}\n" +
+               $"Magazine address: {Address}\n" +
+               $"Magazine description: {Description}\n" +
+               $"Magazine number: {Number}\n" +
+               $"Email: {Email}\n";
+    }
+}
+*/
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
