f1fc0c090ad44751f78a9c21ab3c67dbecf200a1
Ziya Hacili <mziyahajl@mail.ru>
2023-09-18 13:40:21 +0400

    14.09.2023

---
+++ C# homeworks/14.09.2023_2/14.09.2023_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Channels;
+using _14._09._2023_2;
+
+try
+{
+    Calculator calculator = new();
+
+    Calculator.MathOperation addOperation = calculator.Add;
+    Console.WriteLine(addOperation(4, 10));
+
+    Calculator.MathOperation subtractOperation = calculator.Subtract;
+    Console.WriteLine(subtractOperation(10, 3));
+
+    Calculator.MathOperation multiplyOperation = calculator.Multiply;
+    Console.WriteLine(multiplyOperation(3, 2));
+
+    Calculator.MathOperation divideOperation = calculator.Divide;
+    double result = multiplyOperation(10, 0);
+    Console.WriteLine(result);
+    
+    Console.WriteLine(addOperation(12, 321));
+    Console.WriteLine(subtractOperation(3, 3));
+    Console.WriteLine(multiplyOperation(40, 0));
+    Console.WriteLine(divideOperation(45, 5));
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+
+
+
